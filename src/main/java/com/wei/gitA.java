eec21a040d5d6359e7bc074db86b17182a2b9c53
yinjunwei <997449038@qq.com>
2021-07-23 20:20:25 +0800

    12345

--- src/main/java/com/wei/gitA.java
+++ src/main/java/com/wei/gitA.java
@@ -4,5 +4,6 @@ public class gitA {
     public static void main(String[] args) {
         System.out.println("git1");
         System.out.println("git2");
+        System.out.println("git3");
     }
 }
